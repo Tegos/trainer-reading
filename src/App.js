c92a94404c227ed347234249a8db417943e060ec
Ivan Mykhavko Tegos <tegosiv@gmail.com>
2017-09-07 17:50:03 +0300

    Working on Range Slider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import styled from 'styled-components';
 
-import {RaisedButton, Dialog, FlatButton} from 'material-ui';
+import {RaisedButton, Dialog, FlatButton, RadioButton, RadioButtonGroup} from 'material-ui';
 import {MuiThemeProvider} from 'material-ui/styles';
 import Header from './components/Header';
 
 import Slider from 'material-ui/Slider';
 import AppBar from 'material-ui/AppBar';
+import {Range} from 'rc-slider';
 
 injectTapEventPlugin();
 
@@ -46,9 +47,41 @@ class App extends Component {
 					<AppBar title="Trainer of speed reading with marks"
 					        showMenuIconButton={false}/>
 					{header}
-					<h2>Material-UI</h2>
-					<div>
-						{sliderForArea}
+					<Range min={1} value={[2, 30]}/>
+					<table style={{width: '100%'}}>
+						<tr>
+							<td>
+								<div>
+									{sliderForArea}
+								</div>
+							</td>
+							<td></td>
+							<td>
+								<div>
+									{sliderForArea}
+								</div>
+							</td>
+						</tr>
+					</table>
+
+					<div className="row">
+						<div className="col-md-2">
+							Type:
+						</div>
+						<div className="col-md-10">
+							<RadioButtonGroup
+								className="row"
+								name="type">
+								<RadioButton
+									className="col-md-4"
+									value="other"
+									label="Other"/>
+								<RadioButton
+									className="col-md-4"
+									value="custom"
+									label="Custom"/>
+							</RadioButtonGroup>
+						</div>
 					</div>
 					<RaisedButton label="Super Secret Password" secondary={true} onTouchTap={this.handleTouchTap}/>
 
